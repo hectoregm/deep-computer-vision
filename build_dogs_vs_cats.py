2734b256f33f0668d5333d48ce15a6fad74298b3
Hector E. Gomez Morales <hectoregm@gmail.com>
2020-10-29 22:42:31 -0700

    Finish Chapter 9

--- build_dogs_vs_cats.py
+++ build_dogs_vs_cats.py
@@ -1,4 +1,4 @@
-from .config import dogs_vs_cats_config as config
+from config import dogs_vs_cats_config as config
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from pyimagesearch.preprocessing import AspectAwarePreprocessor
@@ -29,3 +29,32 @@ datasets = [
 
 aap = AspectAwarePreprocessor(256, 256)
 (R, G, B) = ([], [], [])
+
+for (dType, paths, labels, outputPath) in datasets:
+    print("[INFO] building {}...".format(outputPath))
+    writer = HDF5DatasetWriter((len(paths), 256, 256, 3), outputPath)
+
+    widgets = ["Building Dataset: ", progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
+    pbar = progressbar.ProgressBar(maxval=len(paths), widgets=widgets).start()
+
+    for (i, (path, label)) in enumerate(zip(paths, labels)):
+        image = cv2.imread(path)
+        image = aap.preprocess(image)
+
+        if dType == "train":
+            (b, g, r) = cv2.mean(image)[:3]
+            R.append(r)
+            G.append(g)
+            B.append(b)
+
+        writer.add([image], [label])
+        pbar.update(i)
+
+    pbar.finish()
+    writer.close()
+
+print("[INFO] serializing means...")
+D = {"R": np.mean(R), "G": np.mean(G), "B": np.mean(B)}
+f = open(config.DATASET_MEAN, "w")
+f.write(json.dumps(D))
+f.close()
